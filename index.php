7f83d85bad333de065c563839fa8291274dcc13c
Alyssia <115212411+alyssiapr@users.noreply.github.com>
2023-11-16 18:44:05 +0100

    Add files via upload
    Rendu final !!

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ Le jeu des billes <br>
 
 <?php
 @include_once("utils.php");
-
- //creer une classe character constituer des attributs nom, billes
-
-//creer une classe hero qui herite de character et qui a en plus des attributs bonus, malus, scream_war
+// creer une classe character avec les attributs name et billes
 
 class Character {
     private $name;
@@ -30,6 +27,7 @@ class Character {
     }
 }
 
+// creer une classe hero qui herite de character avec les attributs bonus, malus et screamWar
 class Hero extends Character {
     private $bonus;
     private $malus;
@@ -55,7 +53,7 @@ class Hero extends Character {
     }
 }
 
-// creer une classe méchant qui hérite de character et qui a comme attribut son age
+
 class Mechant extends Character {
     private $age;
 
@@ -95,12 +93,13 @@ class Mechant extends Character {
         public function createCharacters()
         {
             $player1 = new Hero("Seong Gi-hun", 15, 2, 1, "Wahouuuuu");
-            $player2 = new Hero("Kang Sae-byeok", 25, 1, 2, "LetsgOOOOOO");
+            $player2 = new Hero("Kang Sae-byeok", 25, 1, 2, "Letsgooooo");
             $player3 = new Hero("Cho Sang-woo", 35, 0, 3, "Yihawww");
 
             $this->characters = array($player1, $player2, $player3);
         }
 
+        // Creer une fonction createEnemies qui va creer 20 ennemis avec des noms, des billes aleatoires et des ages aleatoires 
         public function createEnemies()
         {
             $enemy1 = new Mechant("Randall", rand(1, 20), rand(18, 90));
@@ -131,7 +130,7 @@ class Mechant extends Character {
             $enemy17, $enemy18, $enemy19, $enemy20);
         }
     public function startGame()
-    {
+    {//choisir une difficulté aléatoirement
         $randomDifficulty = $this->difficulte[array_rand($this->difficulte)];
             if ($randomDifficulty == "Facile") {
                 $niveau = 5;
@@ -140,22 +139,26 @@ class Mechant extends Character {
             } else {
                 $niveau = 20;
             }
-
-            // Rejouer le jeu autant de fois que le niveau de difficulté
-
+            
         echo "La difficulté est : " . $randomDifficulty;
         echo "<br>";
         $randomHero = $this->characters[array_rand($this->characters)];
+        // choisir un héros aléatoirement
         echo "Votre héros est : " . $randomHero->getName();
         echo "<br>";
         echo "Il possède " . $randomHero->getBilles() . " billes";
+        // Donner le nom de bille du heros aléatoire
         echo "<br>";
         $randomEnemy = $this->enemies[array_rand($this->enemies)];
+        // choisir un ennemi aléatoirement
         echo "Il affrontera " . $randomEnemy->getName();
         echo "<br>";
+        // donner l'age de l'ennemi aléatoire
         echo "Il a " . $randomEnemy->getAge() . " ans";
         echo "<br>";
 
+        // le héros perd une bille tant qu'il en a et qu'il reste des ennemis
+        // le héros gagne si il a plus d'une bille
         while ($randomHero->getBilles() > 0 && count($this->enemies) > 0) {
             $randomHero->setBilles($randomHero->getBilles() - 1);
             echo $randomHero->getName() . " a perdu une bille";
@@ -164,6 +167,9 @@ class Mechant extends Character {
             echo "<br>";
             array_splice($this->enemies, array_rand($this->enemies), 1);
         }
+// Choisir un nombre aléatoire entre 0 et 1, si 0 le héros choisi pair, si 1 le héros choisi impair
+// Si le nombre de billes du héros est pair et qu'il a choisi pair, il gagne une bille
+// Si le nombre de billes du héros est impair et qu'il a choisi impair, il gagne une bille
 
         $guess = Utils::generateRandomNumber(0, 1);
         if ($guess == 0) {
@@ -173,6 +179,8 @@ class Mechant extends Character {
         }
         echo "<br>";
         if ($randomHero->getBilles() > 1) {
+            echo $randomHero->GetScreamWar();
+            echo "<br>";
             echo "La partie est finie, " . $randomHero->getName() . " a gagné !";
         } else {
             echo "La partie est finie, " . $randomHero->getName() . " a perdu !";
@@ -181,13 +189,6 @@ class Mechant extends Character {
     
 }
 
-
 $game = new Game();
-
 $game->startGame();
-// for ($i = 0; $i < $niveau; $i++) {
-//     $game->startGame();
-//     echo "<br>";
-// }
-
 ?>
